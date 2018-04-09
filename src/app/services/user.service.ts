a28ab6ab1db6410e9e99b524b213b312ccfcaa6a
Brett Brunswick <BrettBrunswick@Bretts-MacBook-Pro-2.local>
2018-04-08 20:02:47 -0400

    Users can log in and log out. Must log in before navigating through app

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -63,6 +63,7 @@ export class UserService {
     localStorage.removeItem('token');
     this.loggedIn = false;
     //this._authNavStatusSource.next(false);
+    alert("Goodbye")
   }
 
   isLoggedIn() {
